39946a683520b5d565ca953ab3a9f91a2ae795c5
Lappriou <ludovic.appriou@outlook.fr>
2017-02-01 12:29:48 +0100

    Debug

---
+++ EffetOpacite.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecorateurBouton
+{
+    public class EffetOpacite : IBouton
+    {
+        public IBouton BoutonAvant { get; set; }
+
+        public EffetOpacite(IBouton boutonAvant)
+        {
+            this.BoutonAvant = boutonAvant;
+
+        }
+
+
+        public int GetHauteur()
+        {
+            return BoutonAvant.GetHauteur();
+        }
+
+        public int GetLargeur()
+        {
+            return BoutonAvant.GetLargeur();
+
+        }
+
+        public int GetBouclier()
+        {
+            return BoutonAvant.GetBouclier();
+        }
+
+        public int GetOpacite()
+        {
+            return BoutonAvant.GetOpacite()/2;
+        }
+    }
+}
---
+++ IBouton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecorateurBouton
+{
+    public interface IBouton
+    {
+        int GetHauteur();
+        int GetLargeur();
+
+        int GetBouclier();
+
+        int GetOpacite();
+       
+    }
+}
